e372a22f0b98731d3ca392b5720109e4022fc0f0
JavaCommons Technologies <javacommons@gmail.com>
2021-02-04 12:02:07 +0900

    追加: gtest/md5-test.cpp

--- gtest/gtest.sh
+++ gtest/gtest.sh
@@ -1,7 +1,8 @@
 #! bash -uvx
 pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-toolchain \
-	$MINGW_PACKAGE_PREFIX-gtest
+	$MINGW_PACKAGE_PREFIX-gtest \
+	$MINGW_PACKAGE_PREFIX-openssl
 
 g++ -std=c++11 -o test01 test01.cpp -lgtest_main -lgtest
-test01.exe
+./test01.exe
---
+++ gtest/md5-test.sh
@@ -0,0 +1,2 @@
+#! bash -uvx
+g++ -o md5-test md5-test.cpp -lcrypto && ./md5-test.exe
