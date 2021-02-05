35fa25f4ba8b9aa8516e8992407a8626ddf96ef5
JavaCommons Technologies <javacommons@gmail.com>
2021-02-06 01:01:48 +0900

    修正: gtest/

--- gtest/cov.sh
+++ gtest/cov.sh
@@ -5,11 +5,11 @@ pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-lcov \
 	$MINGW_PACKAGE_PREFIX-openssl
 
-rm -f test01.exe
-g++ -std=c++20 -I .. -fprofile-arcs -ftest-coverage -o test01 test01.cpp -lgtest_main -lgtest -lcrypto
+rm -f test01.exe test01.gcda
+g++ -std=c++20 -fprofile-arcs -ftest-coverage -o test01 test01.cpp -lgtest_main -lgtest -lcrypto
 ./test01.exe
 gcov test01.gcda
-code strconv.h.gcov
-#lcov -c -d . -o test01.lcov.info
-#genhtml test01.lcov.info -o ./info.tmp
-#genhtml test01.lcov.info
+#code strconv.h.gcov
+lcov -c -d . -o test01.lcov.info
+genhtml test01.lcov.info -o ./info.tmp
+start ./info.tmp/c/Users/javac/.software/msys64/mingw32/include/strconv.h.gcov.html
