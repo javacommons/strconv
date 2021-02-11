7a79c0a046e35ed148963f2b6062804293765107
JavaCommons Technologies <javacommons@gmail.com>
2021-02-11 17:10:34 +0900

    modify: gtest/

--- gtest/01.sh
+++ gtest/01.sh
@@ -4,6 +4,8 @@ pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-gtest \
 	$MINGW_PACKAGE_PREFIX-openssl
 
+cp -p ../strconv.h $MINGW_PREFIX/include/
+
 rm -f test01-c++14.exe
 g++ -std=c++14 -o test01-c++14 test01.cpp -lgtest_main -lgtest -lcrypto
 ./test01-c++14.exe
--- gtest/cov.sh
+++ gtest/cov.sh
@@ -5,14 +5,15 @@ pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-lcov \
 	$MINGW_PACKAGE_PREFIX-openssl
 
+cp -p ../strconv.h $MINGW_PREFIX/include/
+
 rm -f test01.exe test01.gcda
 g++ -std=c++20 -fprofile-arcs -ftest-coverage -o test01 test01.cpp -lgtest_main -lgtest -lcrypto
 ./test01.exe
 gcov test01.gcda
 #code strconv.h.gcov
-#lcov -c -d . -o test01.lcov.info
-#genhtml test01.lcov.info -o ./info.tmp
 lcov -c -d . -o test01.lcov
+rm -rf ./info.tmp
 genhtml test01.lcov -o ./info.tmp
 #start info.tmp/include/strconv.h.gcov.html
 start info.tmp/c/Users/javac/.software/msys64/mingw32/include/strconv.h.gcov.html
