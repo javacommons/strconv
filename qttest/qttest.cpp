6225fc75f91f8110c7903a78299b344d6a221561
JavaCommons Technologies <javacommons@gmail.com>
2021-03-17 02:14:11 +0900

    modify: qttest/

--- qttest/qttest.cpp
+++ qttest/qttest.cpp
@@ -9,6 +9,9 @@ private slots:
     void initTestCase();
     void cleanupTestCase();
     void test01();
+    void test02();
+    void test03();
+    void test04();
 };
 
 void TestStrconv::initTestCase()
@@ -27,6 +30,35 @@ void TestStrconv::test01()
     std::wstring ws = L"漢字=한자";
     QString qs = wide_to_qstr(ws);
     QCOMPARE(qs, L"漢字=한자");
+    std::wstring ws2 = qstr_to_wide(qs);
+    QCOMPARE(ws2, L"漢字=한자");
+}
+
+void TestStrconv::test02()
+{
+    QString qs = "漢字=한자";
+    std::string as = qstr_to_ansi(qs);
+    QCOMPARE(as, utf8_to_ansi("漢字=한자"));
+    QString qs2 = ansi_to_qstr(utf8_to_ansi("漢字=한자"));
+    QCOMPARE(qs2, L"漢字=??");
+}
+
+void TestStrconv::test03()
+{
+    QString qs = "漢字=한자";
+    std::string as = qstr_to_sjis(qs);
+    QCOMPARE(as, utf8_to_sjis("漢字=한자"));
+    QString qs2 = sjis_to_qstr(utf8_to_sjis("漢字=한자"));
+    QCOMPARE(qs2, L"漢字=??");
+}
+
+void TestStrconv::test04()
+{
+    QString qs = "漢字=한자";
+    std::string as = qstr_to_utf8(qs);
+    QCOMPARE(as, "漢字=한자");
+    QString qs2 = utf8_to_qstr("漢字=한자");
+    QCOMPARE(qs2, L"漢字=한자");
 }
 
 QTEST_MAIN(TestStrconv)
