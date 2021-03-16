936816af6a5995dad0a963431ec101f1bb27d71f
JavaCommons Technologies <javacommons@gmail.com>
2021-03-17 01:44:19 +0900

    modify: qttest/

--- qttest/qttest.cpp
+++ qttest/qttest.cpp
@@ -1,18 +1,33 @@
 #include <QtTest/QtTest>
 
-class TestQString: public QObject
+#include "strconvQt.h"
+
+class TestStrconv: public QObject
 {
     Q_OBJECT
 private slots:
-    void toUpper();
+    void initTestCase();
+    void cleanupTestCase();
+    void test01();
 };
 
-void TestQString::toUpper()
+void TestStrconv::initTestCase()
 {
-    QString str = "Hello";
-    //QVERIFY(str.toUpper() == "HELLO");
-    QCOMPARE(str.toUpper(), QString("HELLOx"));
+    qDebug() << "initTestCase() called";
 }
 
-QTEST_MAIN(TestQString)
+void TestStrconv::cleanupTestCase()
+{
+    qDebug() << "cleanupTestCase() called";
+    //QFAIL("xyz");
+}
+
+void TestStrconv::test01()
+{
+    std::wstring ws = L"漢字=한자";
+    QString qs = wide_to_qstr(ws);
+    QCOMPARE(qs, L"漢字=한자");
+}
+
+QTEST_MAIN(TestStrconv)
 #include "qttest.moc"
